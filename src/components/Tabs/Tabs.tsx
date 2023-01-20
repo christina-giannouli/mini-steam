0c4859346a810a00064f93f8d81e7a7afb8b6ada
christina-giannouli <xristitina@gmail.com>
2023-01-20 14:53:14 +0200

    Add search functionality

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import Api from '../../api';
 import { App } from '../../types';
+import { TabList } from './tablist';
 
 import Card from '../Card/Card';
 import Loader from '../Loader/Loader';
@@ -9,26 +10,7 @@ import Notification from '../Notification/Notification';
 
 import './Tabs.scss';
 
-// TODO: Move to constants file
-export const TabList = [
-  {
-    name: 'new_and_trending',
-    title: 'New & Trending',
-  },
-  {
-    name: 'top_sellers',
-    title: 'Top Sellers',
-  },
-  {
-    name: 'being_played',
-    title: 'Being Played',
-  },
-  {
-    name: 'upcoming',
-    title: 'Upcoming',
-  },
-];
-
+// TODO: Clean up by moving logic to GameList
 const Tabs = (): JSX.Element => {
   const [activeTab, setActiveTab] = useState<string>(TabList[0].name);
   const [content, setContent] = useState<App[]>([]);
@@ -37,6 +19,8 @@ const Tabs = (): JSX.Element => {
   const [notificationType, setNotificationType] = useState<'danger' | 'info' | 'success'>(
     'success',
   );
+  const [search, setSearch] = useState<string>('');
+  const [hasResults, setHasResults] = useState(true);
 
   const fetchApps = async (): Promise<void> => {
     try {
@@ -60,16 +44,28 @@ const Tabs = (): JSX.Element => {
     setActiveTab(e.target.id);
   };
 
+  const handleSearch = (e: any): void => {
+    const { value } = e.target;
+    setSearch(value);
+
+    const searchResults = content.filter((data) =>
+      data.name.toLowerCase().includes(search.toLowerCase()),
+    );
+    setHasResults(searchResults.length > 0);
+  };
+
   useEffect(() => {
     fetchApps();
+    setSearch('');
+    setHasResults(true);
   }, [activeTab]);
 
   return (
-    <div className="mt-5">
+    <div className="my-5">
       <div className="tabs p-3 d-flex flex-wrap justify-content-between">
         {TabList.map((tab) => (
           <button
-            className="rounded-4 py-2 px-3"
+            className="btn rounded-4 py-2 px-3"
             key={tab.name}
             id={tab.name}
             disabled={activeTab === `${tab.name}`}
@@ -79,6 +75,20 @@ const Tabs = (): JSX.Element => {
         ))}
       </div>
       <div className="px-1 py-5">
+        {/* Search field*/}
+        <div className="row mb-5">
+          <div className="col col-md-4 col-lg-2">
+            <label htmlFor="searchByName" className="form-label d-none" />
+            <input
+              onChange={handleSearch}
+              value={search}
+              type="search"
+              className="form-control"
+              id="searchByName"
+              placeholder="Search by name"
+            />
+          </div>
+        </div>
         {error && <Notification message={error} type={notificationType} />}
         {isLoading ? (
           <Loader />
@@ -86,18 +96,29 @@ const Tabs = (): JSX.Element => {
           TabList.map((tab) => (
             <>
               {activeTab === `${tab.name}` && (
-                <div key={tab.name} className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
-                  {content?.map((game) => (
-                    <div key={game._id} className="col">
-                      <Card
-                        _id={game._id}
-                        name={game.name}
-                        header_image={game.header_image}
-                        price_overview={game.price_overview}
-                        platforms={game.platforms}
+                <div key={tab.name} className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-5">
+                  {hasResults ? (
+                    content
+                      ?.filter((data) => data.name.toLowerCase().includes(search.toLowerCase()))
+                      .map((game) => (
+                        <div key={game._id} className="col">
+                          <Card
+                            _id={game._id}
+                            name={game.name}
+                            header_image={game.header_image}
+                            price_overview={game.price_overview}
+                            platforms={game.platforms}
+                          />
+                        </div>
+                      ))
+                  ) : (
+                    <div className="col offset-sm-3 offset-lg-4">
+                      <Notification
+                        message={'Nothing found :( . Try a different term.'}
+                        type={'info'}
                       />
                     </div>
-                  ))}
+                  )}
                 </div>
               )}
             </>
---
+++ src/components/Tabs/tablist.ts
@@ -0,0 +1,18 @@
+export const TabList: { name: string; title: string }[] = [
+  {
+    name: 'new_and_trending',
+    title: 'New & Trending',
+  },
+  {
+    name: 'top_sellers',
+    title: 'Top Sellers',
+  },
+  {
+    name: 'being_played',
+    title: 'Being Played',
+  },
+  {
+    name: 'upcoming',
+    title: 'Upcoming',
+  },
+];
